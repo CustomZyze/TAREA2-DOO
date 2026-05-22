d51c8808875804a83d969061b5d95f63508bdcef
Maximiliano Pinto <mpinto2025@udec.cl>
2026-05-22 01:02:03 -0400

    feat:crear metodo constructor y metodos abstractos

--- src/main/java/org/example/ReunionVirtual.java
+++ src/main/java/org/example/ReunionVirtual.java
@@ -7,3 +7,17 @@ import java.util.List;
 
 public class ReunionVirtual extends Reunion{
     private String enlace;
+
+    public ReunionVirtual(Date fecha, Instant horaPrevista, Duration duracionPrevista, String enlace){
+        super(fecha, horaPrevista, duracionPrevista);
+        this.enlace = enlace;
+    }
+
+    @Override
+    public List<Asistencia> obtenerAsistencias() {
+        return new ArrayList<>();
+    }
+
+    @Override
+    public List<Invitacion> obtenerAusencias(){
+        return new ArrayList<>();
