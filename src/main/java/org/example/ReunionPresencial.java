6b912923870c9c90942c8195285945605eaf813b
Maximiliano Pinto <mpinto2025@udec.cl>
2026-05-22 01:38:19 -0400

    feat:crear metodo constructor y metodos abstractos

--- src/main/java/org/example/ReunionPresencial.java
+++ src/main/java/org/example/ReunionPresencial.java
@@ -7,3 +7,17 @@ import java.util.List;
 
 public class ReunionPresencial extends Reunion{
     private String sala;
+
+    public ReunionPresencial(Date fecha, Instant horaPrevista, Duration duracionPrevista, String sala){
+        super(fecha, horaPrevista, duracionPrevista);
+        this.sala=sala;
+    }
+
+    @Override
+    public List<Asistencia> obtenerAsistencias(){
+        return new ArrayList<>();
+    }
+
+    @Override
+    public List<Invitacion> obtenerAusencias(){
+        return new ArrayList<>();
