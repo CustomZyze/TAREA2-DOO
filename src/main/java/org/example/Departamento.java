23c15b57dad627cd2f26671e6eba273254c6813a
Matias Cabello <maticab170@gmail.com>
2026-05-22 16:09:21 -0400

    feat: implementar interfaz Invitable, getters/setters y toString a Empleado y Departamento

--- src/main/java/org/example/Departamento.java
+++ src/main/java/org/example/Departamento.java
@@ -2,11 +2,44 @@ package org.example;
 import java.util.ArrayList;
 import java.util.List;
 
-public class Departamento{
+public class Departamento implements Invitable{
     private String nombre;
     private List<Empleado> empleados;
-    public Departamento(String nombre) {
+    public Departamento(String nombre){
         this.nombre = nombre;
         this.empleados = new ArrayList<>();
+    }
+
+    public void añadirEmpleado(Empleado emp){
+        this.empleados.add(emp);
+    }
+
+    public int obtenerCantidadEmpleados(){
+        return this.empleados.size();
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public void setEmpleados(List<Empleado> empleados) {
+        this.empleados = empleados;
+    }
+
+    @Override
+    public String toString(){
+        String info = "Depto: " + nombre + "(Cant. Empleados: " + obtenerCantidadEmpleados() + ")\n";
+        for (Empleado emp : empleados){
+            info = info + " -" + emp.toString() + "\n";
+        }
+        return info;
+    }
+
+    @Override
+    public void invitar() {
     }
 }
--- src/main/java/org/example/Empleado.java
+++ src/main/java/org/example/Empleado.java
@@ -1,6 +1,6 @@
 package org.example;
 
-public class Empleado{
+public class Empleado implements Invitable{
     private String id;
     private String apellido;
     private String nombre;
@@ -11,6 +11,27 @@ public class Empleado{
         apellido = b;
         nombre = c;
         correo = d;
+    }
+
+    public String getId() { return id; }
+    public void setId(String id) { this.id = id; }
+
+    public String getApellidos() { return apellido; }
+    public void setApellidos(String apellidos) { this.apellido = apellidos; }
+
+    public String getNombre() { return nombre; }
+    public void setNombre(String nombre) { this.nombre = nombre; }
+
+    public String getCorreo() { return correo; }
+    public void setCorreo(String correo) { this.correo = correo; }
+
+    @Override
+    public  String toString(){
+        return "Empleado [" + id + "]: " + nombre + " " + apellido + " " + correo;
+    }
+
+    @Override
+    public void invitar() {
     }
 }
 
