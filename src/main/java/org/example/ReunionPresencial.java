031e59c7d5aef175b82f2ee6d06e94d525327d4a
Maximiliano Pinto <mpinto2025@udec.cl>
2026-05-22 01:37:00 -0400

    feat: crear clase ReunionPresencial y su atributo

---
+++ src/main/java/org/example/ReunionPresencial.java
@@ -0,0 +1,9 @@
+package org.example;
+
+import java.util.Date;
+import java.time.Instant;
+import java.time.Duration;
+import java.util.List;
+
+public class ReunionPresencial extends Reunion{
+    private String sala;
