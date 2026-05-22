070e1a20acfde7e4f856011579b2538f6b646bd4
Maximiliano Pinto <mpinto2025@udec.cl>
2026-05-21 23:27:35 -0400

    feat:crear clase abstracta reunion y sus atributos

---
+++ src/main/java/org/example/Reunion.java
@@ -0,0 +1,14 @@
+package org.example;
+
+import java.util.Date;
+import java.time.Instant;
+import java.time.Duration;
+import java.util.List;
+
+public abstract class Reunion {
+
+    private Date fecha;
+    private Instant horaPrevista;
+    private Duration duracionPrevista;
+    private Instant horaInicio;
+    private Instant horaFin;
