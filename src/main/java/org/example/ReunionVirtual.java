95b63a7e2748997b4bdb9b73d07222091d61fe99
Maximiliano Pinto <mpinto2025@udec.cl>
2026-05-22 01:00:41 -0400

    feat:crear clase ReunionVirtual y su atributo

---
+++ src/main/java/org/example/ReunionVirtual.java
@@ -0,0 +1,9 @@
+package org.example;
+
+import java.util.Date;
+import java.time.Instant;
+import java.time.Duration;
+import java.util.List;
+
+public class ReunionVirtual extends Reunion{
+    private String enlace;
