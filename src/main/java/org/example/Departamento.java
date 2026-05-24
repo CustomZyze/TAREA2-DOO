c9dfbf8ff27ff85b9bd77354e993f237d87d8c20
Matias Cabello <maticab170@gmail.com>
2026-05-23 23:47:26 -0400

    docs: Añadir documentacion javadoc a Asistencia, Departamento, Empleado, InformeReunion, InvExterno e Invitable

--- src/main/java/org/example/Departamento.java
+++ src/main/java/org/example/Departamento.java
@@ -2,14 +2,30 @@ package org.example;
 import java.util.ArrayList;
 import java.util.List;
 
+/**
+ * Departamento dentro de la empresa, el cual agrupa a los empleados
+ * Implementa la interfaz Invitable para permitir que se invite a todo el departamento de una.
+ */
 public class Departamento implements Invitable{
+    /** El nombre del departamento */
     private String nombre;
+    /** Lista de empleados que son parte del departamento */
     private List<Empleado> empleados;
+    /**
+     * Crea un nuevo Departamento y establece una lista vacía de empleados
+     *
+     * @param nombre El nombre del departamento.
+     */
     public Departamento(String nombre){
         this.nombre = nombre;
         this.empleados = new ArrayList<>();
     }
-
+    /**
+     * Añade un nuevo empleado a la lista del departamento
+     *
+     * @param emp El objeto Empleado que se desea añadir
+     * @throws ParticipanteNuloException Si se intenta añadir un empleado con valor nulo.
+     */
     public void añadirEmpleado(Empleado emp) throws ParticipanteNuloException{
         if (emp == null) {
             throw new ParticipanteNuloException("No se puede añadir un Empleado null al depto.");
@@ -17,6 +33,11 @@ public class Departamento implements Invitable{
         this.empleados.add(emp);
     }
 
+    /**
+     * Obtiene el total de empleados que pertenecen al departamento
+     *
+     * @return La cantidad de empleados en la lista.
+     */
     public int obtenerCantidadEmpleados(){
         return this.empleados.size();
     }
@@ -33,6 +54,12 @@ public class Departamento implements Invitable{
         this.empleados = empleados;
     }
 
+    /**
+     * Devuelve en texto del departamento, incluyendo su nombre,
+     * la cantidad de empleados y los datos de cada uno de ellos mediante un ciclo for.
+     *
+     * @return Una cadena de texto con la información detallada del departamento.
+     */
     @Override
     public String toString(){
         String info = "Depto: " + nombre + "(Cant. Empleados: " + obtenerCantidadEmpleados() + ")\n";
@@ -42,6 +69,11 @@ public class Departamento implements Invitable{
         return info;
     }
 
+    /**
+     * Simula la invitación del departamento completo a una reunión cualquiera
+     * Imprime un mensaje en consola para el departamento y usa el metodo invitar
+     * de cada uno de los empleados que lo componen.
+     */
     @Override
     public void invitar() {
         System.out.println("Invitando a depto: " + getNombre());
--- src/main/java/org/example/Invitable.java
+++ src/main/java/org/example/Invitable.java
@@ -1,5 +1,10 @@
 package org.example;
 
+/**
+ * Interfaz que define como se comporta cualquier cosa que pueda ser
+ * invitada a una reunión (como empleados individuales, departamentos completos
+ * o invitados externos).
+ */
 public interface Invitable {
     void invitar();
 }
