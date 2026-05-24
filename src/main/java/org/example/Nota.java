7dcdd3e2bac5f9eb93349d8dc854713762d8e534
Paulo <paulotorrescavila@gmail.com>
2026-05-23 23:56:19 -0400

    doc: añadir javadoc de clases Nota, Persona y Retraso

--- src/main/java/org/example/Nota.java
+++ src/main/java/org/example/Nota.java
@@ -1,19 +1,46 @@
 package org.example;
 
+/**
+ * Representa una nota asociada a una reunión la cual
+ * permite guardar información, comentarios o
+ * acuerdos importantes relacionados con una reunión.
+ */
 public class Nota {
+
+    /**
+     * Contenido o texto de la nota.
+     */
     private String contenido;
 
+
+    /**
+     * Crea una nueva nota con el contenido indicado.
+     * @param contenido texto o información que tendrá la nota.
+     */
     public Nota(String contenido){
         this.contenido = contenido;
     }
+
+    /**
+     * Obtiene el contenido de la nota.
+     * @return contenido de la nota.
+     */
     public String getContenido(){
         return contenido;
     }
 
+    /**
+     * Modifica el contenido de la nota.
+     * @param contenido nuevo contenido de la nota.
+     */
     public void setContenido(String contenido){
         this.contenido = contenido;
     }
 
+    /**
+     * Entrega una representación en texto de la nota.
+     * @return contenido de la nota.
+     */
     @Override
     public String toString(){
         return contenido;
